0f438a67f73b3fd34fe1bf3a2397043b18373bc7
stresslimit <colin.vernon@gmail.com>
2021-05-09 22:52:43 -0400

    tweak for launch, add many songs

--- _js/index.js
+++ _js/index.js
@@ -3,6 +3,7 @@
 window.addEventListener('load', (e) => {
 
   externalLinks()
+  song()
 
 })
 
@@ -13,4 +14,23 @@ function externalLinks() {
     if (el.host != window.location.host)
       el.target = "_blank"
   })
+}
+
+
+function song() {
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbz4i7lqu9TNvJho_0HB26ZmzbDeQBnJzDeHkAkl4JNJyDU2oq2QKwd-yRuYhqY_R7UV/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  console.log(form)
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    form.submitbutton.disabled = true
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        console.log('Success!', response)
+        form.innerHTML = 'Thanks!!'
+      })
+      .catch(error => console.error('Error!', error.message))
+    })
 }
