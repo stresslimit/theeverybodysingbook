67a1d683a5b9cad83444a09d55bdd35ca092d939
stresslimit <colin@slowfactory.com>
2025-06-27 12:52:11 -0400

    more

---
+++ _pages/when-the-man-comes-around.markdown
@@ -0,0 +1,106 @@
+---
+title: When the Man Comes Around
+---
+
+
+There's a man, goin' 'round, takin' names
+C/G
+And he decides, who to free, and who to blame
+C/G
+Everybody, won't be treated, all the same
+C                            **           Am
+There'll be a golden ladder, reaching down
+F           G             C/G
+  When the man comes around
+
+C/G
+The hairs, on your arm, will stand up
+C/G
+At the terror, in each sip, and in each sup
+C/G
+Will you partake of that last offered cup?
+C/G                             **    Am
+Or disappear, into the potter's ground?
+F           G             C/G
+  When the man comes around
+
+
+C
+Hear the trumpets, hear the pipers
+C
+One hundred million angels singin'
+C                               F   C    G
+Multitudes are marchin' to the big kettledrum
+G
+Voices callin', voices cryin'
+G
+Some are born and some are dyin'
+G                              C
+It's Alpha and Omega's kingdom come
+
+
+
+[Bridge]
+(C)      F                   C
+And the whirlwind is in the thorn tree
+        C/G
+And the virgins are all trimming their wicks
+     F                   C
+The whirlwind is in the thorn tree
+     C/G
+It's hard for thee to kick against the pricks
+
+
+[Verse 2]
+---------
+C/G
+Till Armageddon, no shalam, no shalom
+C/G
+Then the father hen will call his chickens home
+C/G
+The wise men will bow down before the throne
+C/G                                **      Am
+And at his feet they'll cast their golden crowns…
+F           G              C/G
+  When the man comes around
+
+C/G
+Whoever is unjust, let him be unjust still
+C/G
+Whoever is righteous, let him be righteous still
+C/G
+Whoever is filthy, let him be filthy still
+C/G                        **        Am
+Listen to the words long written down
+F           G              C
+  When the man comes around
+
+
+C
+Hear the trumpets, hear the pipers
+C
+One hundred million angels singing
+C                               F   C    G
+Multitudes are marchin' to the big kettledrum
+G
+Voices callin', voices cryin'
+G
+Some are born and some are dyin'
+G                              C
+It's Alpha and Omega's kingdom come
+
+
+[Bridge]
+(C)      F                   C
+And the whirlwind is in the thorn tree
+C/G
+And the virgins are all trimming their wicks
+F                   C
+The whirlwind is in the thorn tree
+     C/G
+It's hard for thee to kick against the pricks
+
+   C/G                                Am
+In measured a hundredweight and penny pound
+F           G             C/G
+  When the man comes around
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,32 @@
-window.addEventListener("load",(function(o){var e;document.querySelectorAll("a").forEach((function(o){o.host!=window.location.host&&(o.target="_blank")})),e=document.forms["submit-to-google-sheet"],console.log(e),e.addEventListener("submit",(function(o){o.preventDefault(),e.submitbutton.disabled=!0,fetch("https://script.google.com/macros/s/AKfycbz4i7lqu9TNvJho_0HB26ZmzbDeQBnJzDeHkAkl4JNJyDU2oq2QKwd-yRuYhqY_R7UV/exec",{method:"POST",body:new FormData(e)}).then((function(o){console.log("Success!",o),e.innerHTML="Thanks!!"})).catch((function(o){return console.error("Error!",o.message)}))}))}));
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./_js/index.js":
+/*!**********************!*\
+  !*** ./_js/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("// import _ from '_'\n\nwindow.addEventListener('load', function (e) {\n  externalLinks();\n  song();\n});\nfunction externalLinks() {\n  var a = document.querySelectorAll('a');\n  a.forEach(function (el) {\n    if (el.host != window.location.host) el.target = \"_blank\";\n  });\n}\nfunction song() {\n  var scriptURL = 'https://script.google.com/macros/s/AKfycbz4i7lqu9TNvJho_0HB26ZmzbDeQBnJzDeHkAkl4JNJyDU2oq2QKwd-yRuYhqY_R7UV/exec';\n  var form = document.forms['submit-to-google-sheet'];\n  console.log(form);\n  form.addEventListener('submit', function (e) {\n    e.preventDefault();\n    form.submitbutton.disabled = true;\n    fetch(scriptURL, {\n      method: 'POST',\n      body: new FormData(form)\n    }).then(function (response) {\n      console.log('Success!', response);\n      form.innerHTML = 'Thanks!!';\n    })[\"catch\"](function (error) {\n      return console.error('Error!', error.message);\n    });\n  });\n}\n\n//# sourceURL=webpack://theeverybodysingbook/./_js/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./_js/index.js"]();
+/******/ 	
+/******/ })()
+;
